56d749f9f711dca90566732a01aafd25662ebe35
paraIncog <117535473+paraIncog@users.noreply.github.com>
2025-05-12 02:58:45 +0300

    testin gh-pages 1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/err-tryout-work/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
